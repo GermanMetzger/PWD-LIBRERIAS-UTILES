eb6d7e51b8237765718a219a55b4ce25cef6d895
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-10-15 19:36:03 -0300

    logo-titulo

--- app/views/logIn.php
+++ app/views/logIn.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LOGIN</title>
+    <link rel="icon" type="image/jpg" href="./img/favicon.jpg" />
+    <title>LogIn</title>
 </head>
 
 <body>
