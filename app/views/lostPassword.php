3a7130d56e9a5a34388c4938636a1f19d50d4469
Martin Hernandez <martinalejandrohernandez1@gmail.com>
2024-10-16 09:03:10 -0300

    recupero password

--- app/views/lostPassword.php
+++ app/views/lostPassword.php
@@ -4,43 +4,51 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../assets/css/header&footer.css">
-    <link rel="stylesheet" href="../../assets/css/formulario.css">
-    <title>RESTABLECER CONTRASEÑA</title>
+    <link rel="icon" type="image/jpg" href="./img/favicon.jpg" />
+    <title>Restablecer Password</title>
 </head>
 
 <body>
-    <header>
-        <h1>RESTABLECER CONTRASEÑA</h1>
-        <nav>
-            <a href="../../index.php"><h3>Volver</h3></a>
-        </nav>
+    <?php
+    include_once('./structure/header.php');
+    include_once("../../config.php");
+    ?>
 
-    </header>
-    <div class="container">
-        <?php
-        include_once('../../assets/structure/header.php');
-        ?>
+<main>
+        <div class="container card-container d-flex justify-content-center align-items-center" style="height: 80vh">
+            <div class="card text-center bg-dark text-light" style="width: 68rem;">
+                <div class="card-header">
+                    <img src="./img/PHPMailer.jpg" alt="logo" style="height: 50px;">
+                    <h4>Restablecer Password</h4>
+                </div>
+                <div class="card-body">
+                    <form action="actions/recuperacionAction.php" class="needs-validation" method="POST">
+                        <!-- envio por oculto el tipo de verificacion que quiero usar -->
+                        <input type="hidden" name="tipoVerificacion" value='2'>
+                        <!-- -------------------------------------------------------- -->
+                        <div class="mb-3 row justify-content-center">
+                            <div class="col-md-6 col-lg-4">
+                                <label for="email">Email:</label>
+                                <input type="email" class="form-control form-control-sm" name="email" id="email" required>
+                                <div class="invalid-feedback">
+                                    Por favor, ingresá tu Email.
+                                </div>
+                            </div>
+                        </div>
 
-        <main>
-            <div class="centro">
-
-                <form action="actions/recuperacionAction.php" class="form-container" method="POST">
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" id="email">
-                    
-                    <input type="submit" value="Enviar">
-                    
-                </form>
-
-
+                        <div class="mb-3 row justify-content-center">
+                            <div class="col-md-6 col-lg-4">
+                                <input type="submit" class="btn btn-warning btn-sm w-100" value="Enviar">
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </main>
+        </div>
+    </main>
 
-        <?php
-        include_once('../../assets/structure/footer.php');
-        ?>
-    </div>
+    <?php
+    include_once('./structure/footer.php');
+    ?>
 </body>
-
 </html>
